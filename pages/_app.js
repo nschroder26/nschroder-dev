9ed64a3839f485a10327e5f50e791e7c13f32fae
Nicholas Schroder <nrs4492@hotmail.com>
2022-09-13 20:41:19 -0400

    add meta tags. add tab title. add sharp for image load optimization

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,23 @@
 import Layout from "../components/Layout/Layout";
+import Head from "next/head";
+import { Fragment } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Fragment>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="keywords" content="titla, meta, nextjs" />
+        <meta name="author" content="Nick Schroder" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>NS | Dev</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Fragment>
   );
 }
 
